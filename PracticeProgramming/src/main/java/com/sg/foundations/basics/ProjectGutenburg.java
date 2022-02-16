2c9e7de714144cd3467a4d116448f01fb27a1d70
Paul van Sittert <paul.d.vansittert@gmail.com>
2022-02-16 01:04:38 +0000

    Exercise 1

---
+++ PracticeProgramming/src/main/java/com/sg/foundations/basics/ProjectGutenburg.java
@@ -0,0 +1,17 @@
+package com.sg.foundations.basics;
+
+/**
+ *
+ * @author Paul van Sittert
+ */
+public class ProjectGutenburg {
+    public static void main(String[] args) {
+        System.out.print("Did you know that in 1440 (or thereabouts),");
+        System.out.println("Johannes Gutenberg invented the printing press with moveable type?");
+        System.out.println("He started out as a goldsmith!");
+        System.out.print("His invention made it easy to print and");
+        System.out.println("distribute books to anyone who wanted one.");
+        System.out.print("We are like a modern Gutenberg,");
+        System.out.println("printing whatever we want to the console with ease.");
+    }
+}
